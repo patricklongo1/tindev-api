6c3eea839d8ed107890d8448517834ed9d0214b0
Longo <plongo@indracompany.com>
2019-12-28 15:29:13 -0300

    done

--- src/server.js
+++ src/server.js
@@ -4,15 +4,32 @@ import mongoose from 'mongoose';
 
 import routes from './routes';
 
-const server = express();
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+    const { user } = socket.handshake.query;
+    console.log(user, socket.id);
+    connectedUsers[user] = socket.id;
+});
 
 mongoose.connect('mongodb://localhost:27017/tindev', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
 
-server.use(cors());
-server.use(express.json());
-server.use(routes);
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 server.listen(3333);
